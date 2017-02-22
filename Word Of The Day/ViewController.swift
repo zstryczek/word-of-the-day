30482be972b145fac3e50e16d6c1757011d4988d
Student <student@zstryczek.local>
2017-02-22 09:22:43 -0600

    Initial Commit

---
+++ Word Of The Day/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Word Of The Day
+//
+//  Created by zstryczek on 2/22/17.
+//  Copyright © 2017 zstryapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
